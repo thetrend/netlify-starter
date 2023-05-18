c87d3f625675a412fe6ea2c4d3b63398a5a0018d
Grace de la Mora <thetrend@users.noreply.github.com>
2023-05-17 20:54:13 -0400

    Finishing up the MVP

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
+import Error from './Error'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/:displayName?',
+    element: <App />,
+    errorElement: <Error />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
